2118a3f3b735b3c067b28d4d5b9ff7e369ad82fc
Areej Shareefi <aomsh1420@gmail.com>
2025-03-06 22:58:20 +0300

    display products cards

---
+++ frontend/Ecommerce/src/app/product-list/product-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';  // Import ProductService
+import { Router } from '@angular/router';  // Import Router for navigation to product details
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-product-list',
+  imports:[CommonModule,RouterOutlet],
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: any[] = [];  // To hold the list of products
+  errorMessage: string = '';  // To display any errors if fetching data fails
+
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data =>
+        {
+          this.products=data;
+        },
+      (error) => {
+        this.errorMessage = 'Error fetching product data';  // Handle any errors
+        console.error(error);
+      }
+    );
+  }
+
+  // Method to navigate to product details page
+ /*  viewProductDetails(productId: number): void {
+    this.router.navigate(['/product-details', productId]);  // Navigate to product details page with the product ID
+  } */
+}
